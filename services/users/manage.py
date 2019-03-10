7849f9c2af69cda07f2f82a3e588c7c42a0ad7a6
Brent Gawryluik <bgawryluik@appneta.com>
2019-03-09 23:11:59 -0800

    Added Flask Blueprints

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,13 +1,16 @@
 # services/users/manage.py
 
+
 import unittest
 
 from flask.cli import FlaskGroup
 
-from project import app, db
+from project import create_app, db   # new
+from project.api.models import User  # new
 
+app = create_app()  # new
+cli = FlaskGroup(create_app=create_app)  # new
 
-cli = FlaskGroup(app)
 
 @cli.command('recreate_db')
 def recreate_db():
